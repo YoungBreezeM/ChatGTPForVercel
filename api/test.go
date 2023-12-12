e28dca93814474258d619590aea339a51dda7867
yqf <940695836@qq.com>
2023-12-12 16:11:53 +0800

    添加test

---
+++ api/test.go
@@ -0,0 +1,23 @@
+package api
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func Test(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/event-stream")
+	w.Header().Set("Cache-Control", "no-cache")
+	w.Header().Set("Connection", "keep-alive")
+
+	// 设置跨域访问
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+
+	for i := 0; i < 10; i++ {
+
+		time.Sleep(time.Second * 1)
+		fmt.Fprintf(w, "data: Initial data\n\n")
+		w.(http.Flusher).Flush()
+	}
+}
